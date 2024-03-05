841e7640c716b462b87f365fba33b5298b9de639
FaizBano <159723889+FaizBano@users.noreply.github.com>
2024-03-05 08:09:20 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+console.log("Thank you governor sindh Sir Kamran Khan Tessor");
+console.log("I appricate your efforts and admire your work regariding the youth of Sindh as well as Pakisstan ");
+console.log("you are the precious asset of Pakistan");
